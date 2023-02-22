b2a19c9675f31c1a0c5df335da7403e4d876fbf0
naturenono <princeigajah@gmail.com>
2023-02-22 07:27:13 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+include "main.h"
+
+/**
+ * print_sign - print sign of number
+ * @n: argument passed
+ * Return: Always 0
+*/
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
