ef408b69cfcdaf134e54281563f2e63f5f4cc8b8
naturenono <princeigajah@gmail.com>
2023-02-22 13:33:44 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,16 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
 		for (j = 0; j <= 59; j++)
 		{
-			if (i < 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				_putchar(':');
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
