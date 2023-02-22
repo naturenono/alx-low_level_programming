a4879f3cbdea46ed4a12f719b4bb390cb1652556
naturenono <princeigajah@gmail.com>
2023-02-22 12:37:49 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,16 @@
  * @n: the passed args
  * Return: the last digit
 */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
-	int x
+	int x;
 
 	if (n < 0)
-		n = -n;
+	n = -n;
+
 	x = n % 10;
-	_putchar(x +'0');
+
+	_putchar(x + '0');
+
 	return (x);
 }
