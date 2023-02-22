cecb8899a8fcf489bc491f0cbcf9635462253ab7
naturenono <princeigajah@gmail.com>
2023-02-22 06:51:30 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if the character is lowercase
+ * @c: a character argument
+ * Return: Always 0
+*/
+
+int _islower(int c);
+{
+	return (c >= 'a' && c <= 'z');
+}
