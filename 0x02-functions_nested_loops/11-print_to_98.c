b1736b3fd15c7bc3cc06d7861d956f0c963f4f1e
naturenono <princeigajah@gmail.com>
2023-02-22 17:06:54 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - prints all numbers from input to 98
- * @n: the starting number
+ * 
+ * @n: fuction parameter
+ *
  * Return: Always 0
 */
+
 void print_to_98(int n)
 {
 	int i;
