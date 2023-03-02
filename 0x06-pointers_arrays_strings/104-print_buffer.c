a388949310a3e9f15cde318b9c88a21a63c9fb6f
naturenono <princeigajah@gmail.com>
2023-03-01 17:52:23 -0800

    taskses

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,6 +14,7 @@ void print_buffer(char *b, int size)
 	int o, j, i;
 
 	o = 0;
+	
 	if (size <= 0)
 	{
 		printf("\n");
@@ -24,7 +25,7 @@ void print_buffer(char *b, int size)
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
-	{
+		{
 		if (i < j)
 			printf("%02x", *(b + o + i));
 		else
@@ -40,7 +41,7 @@ void print_buffer(char *b, int size)
 
 		if (c < 32 || c > 132)
 		{
-		c = '.';
+			c = '.';
 		}				                        printf("%c", c);
 	}
 	printf("\n");
