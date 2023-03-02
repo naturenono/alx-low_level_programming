fa2a8963f42b3143696ddf3115f4f78f606e4320
naturenono <princeigajah@gmail.com>
2023-03-01 17:43:47 -0800

    taskses

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -53,7 +53,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-		val1 = 0;
+			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
 		if (j < 0)
@@ -73,7 +73,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		i--;
 	}
 	if (digits == size_r)
-	return (0);
+		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
