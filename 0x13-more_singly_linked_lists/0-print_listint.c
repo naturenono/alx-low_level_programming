88484b8aad4a91434ade7a481229bdfe6ed6de3d
naturenono <princeigajah@gmail.com>
2023-04-04 08:45:26 -0700

    0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- n++;
- h = h->next;
- }
-
- return (n);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
